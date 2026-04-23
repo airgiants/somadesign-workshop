299f87e9448951844b00fbd08f0e2e3c76ff3ad0
Robert Nixdorf <rob@rnix.de>
2026-04-23 11:20:01 +0100

    Add MQTT client functionality and message publishing
    Implement MQTT client with connection and reconnection logic.

---
+++ mqtt.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python3
+
+from paho.mqtt import client as mqtt_client
+
+broker = 'mqtt.airgiants.co.uk'
+port = 1883
+topic = "airgiants"
+client_id = f'python-mqtt-{random.randint(0, 1000)}'
+# username = 'emqx'
+# password = 'public'
+
+def connect_mqtt():
+    def on_connect(client, userdata, flags, rc):
+    # For paho-mqtt 2.0.0, you need to add the properties parameter.
+    # def on_connect(client, userdata, flags, rc, properties):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+    # Set Connecting Client ID
+    client = mqtt_client.Client(client_id)
+    
+    # For paho-mqtt 2.0.0, you need to set callback_api_version.
+    # client = mqtt_client.Client(client_id=client_id, callback_api_version=mqtt_client.CallbackAPIVersion.VERSION2)
+    
+    # client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
+
+FIRST_RECONNECT_DELAY = 1
+RECONNECT_RATE = 2
+MAX_RECONNECT_COUNT = 12
+MAX_RECONNECT_DELAY = 60
+
+def on_disconnect(client, userdata, rc):
+    logging.info("Disconnected with result code: %s", rc)
+    reconnect_count, reconnect_delay = 0, FIRST_RECONNECT_DELAY
+    while reconnect_count < MAX_RECONNECT_COUNT:
+        logging.info("Reconnecting in %d seconds...", reconnect_delay)
+        time.sleep(reconnect_delay)
+
+        try:
+            client.reconnect()
+            logging.info("Reconnected successfully!")
+            return
+        except Exception as err:
+            logging.error("%s. Reconnect failed. Retrying...", err)
+
+        reconnect_delay *= RECONNECT_RATE
+        reconnect_delay = min(reconnect_delay, MAX_RECONNECT_DELAY)
+        reconnect_count += 1
+    logging.info("Reconnect failed after %s attempts. Exiting...", reconnect_count)
+  
+client.on_disconnect = on_disconnect
+
+
+ def publish(client,node,msg):
+     msg_count = 1
+     #while True:
+     if True:
+        # time.sleep(1)
+        # msg = f"messages: {msg_count}"
+         a = "airgiants/hexagorgon/debug/out/hexagorgon/" + node + "/knobs/setDrive"
+         result = client.publish(topic, msg)
+         # result: [0, 1]
+         status = result[0]
+         if status == 0:
+             print(f"Send `{msg}` to topic `{topic}`")
+         else:
+             print(f"Failed to send message to topic {topic}")
+         msg_count += 1
+         if msg_count > 5:
+             break   
+
+
+def run():
+    client = connect_mqtt()
+    client.on_disconnect = on_disconnect
+    for x in range(1,6):
+      publish(client,x,"1.0")
+    time.sleep(1)
+    for x in range(1,6):
+      publish(client,x,"-1.0")
+
+
+
+if __name__ == '__main__':
+    run()
+
+
+
